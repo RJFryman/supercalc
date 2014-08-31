9faaa8f87ba83586cc7fb5b605507a8ff4c46bee
Robert Fryman <robert.fryman@gmail.com>
2014-08-30 20:22:40 -0500

    removed unused qunit

--- public/test/js/test.js
+++
@@ -1,4 +0,0 @@
-test('containsChar',function(){
-  deepEqual(containsChar('mouse','u'),true ,'the letter u should be in mouse');
-  deepEqual(containsChar('mouse','z'),false ,'the letter z should not be in mouse');
-});
